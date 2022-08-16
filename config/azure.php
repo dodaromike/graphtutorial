5bd8cc81d7cfecac8a87b8d079ec577e98c440be
Mike Dodaro (Steyer Associates Inc) <v-dodaromike@microsoft.com>
2022-08-16 09:37:53 -0700

    Add Route::get('/signout', 'AuthController@signout'); to web.php

--- config/azure.php
+++ config/azure.php
@@ -6,8 +6,8 @@
 // This will avoid issues when using Laravel's config caching
 // https://laravel.com/docs/8.x/configuration#configuration-caching
 return [
-  'appId'             => env('OAUTH_APP_ID', '1678ec80-5be0-49e9-8e23-2dc8a2f855f2'),
-  'appSecret'         => env('OAUTH_APP_SECRET', '3~j8Q~b35mY5Aw3A_7LG-zMYkw2ZeDG~ShTxUaU9'),
+  'appId'             => env('OAUTH_APP_ID', ''),
+  'appSecret'         => env('OAUTH_APP_SECRET', ''),
   'redirectUri'       => env('OAUTH_REDIRECT_URI', ''),
   'scopes'            => env('OAUTH_SCOPES', ''),
   'authority'         => env('OAUTH_AUTHORITY', 'https://login.microsoftonline.com/common'),
